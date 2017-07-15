8b7ea4940d41a64d9c1e224ff738a94b19c5c3a0
oott123 <git@public.oott123.com>
2017-07-15 21:03:47 +0800

    add sample packets

--- examples/test.js
+++ examples/test.js
@@ -1,17 +1,21 @@
 const Room = require('../lib/room')
 
-const room = new Room(10003)
+const room = new Room(10029)
 room.on('error', function (err) {
   console.error(err)
 })
 room.on('connect', function () {
   console.log('room connected!')
 })
+room.on('room-message', function (message) {
+  // console.dir(message, {depth: null, breakLength: Infinity, colors: true})
+  console.log(JSON.stringify(message))
+})
 room.on('room-danmaku', function ({data}) {
-  console.log(`${data.from.nickName}: ${data.content}`)
+  // console.log(`${data.from.nickName}: ${data.content}`)
 })
 room.on('meta-update', function (meta) {
-  console.log(`当前：${meta.audiences} 观众，${meta.exp} 经验，${meta.bamboo} 竹子`)
+  // console.log(`当前：${meta.audiences} 观众，${meta.exp.val} 经验，${meta.bamboo} 竹子`)
 })
 console.log('room connecting ...')
 room.join()
